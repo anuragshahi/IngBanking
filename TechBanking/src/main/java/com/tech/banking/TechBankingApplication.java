09b3a09eb326602e17d2802b3489eb2be8f658ff
Anurag Shahi <anurag.shahi111@gmail.com>
2019-09-10 11:54:52 +0530

    First Commit

---
+++ TechBanking/src/main/java/com/tech/banking/TechBankingApplication.java
@@ -0,0 +1,13 @@
+package com.tech.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechBankingApplication.class, args);
+	}
+
+}
