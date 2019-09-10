e0cbe7badb648d7d1645487fee8b566a1a346fa0
Anurag Shahi <anurag.shahi111@gmail.com>
2019-09-10 13:19:08 +0530

    Test Web App

--- TechBanking/src/main/java/com/tech/banking/TechBankingApplication.java
+++ TechBanking/src/main/java/com/tech/banking/TechBankingApplication.java
@@ -2,12 +2,19 @@ package com.tech.banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TechBankingApplication {
+public class TechBankingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechBankingApplication.class, args);
 	}
+	
+	  @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(TechBankingApplication.class);
+	    }
 
 }
